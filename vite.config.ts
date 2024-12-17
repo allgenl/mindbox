93b5dd86b2e154dc381f19f6aaa2f1ca0a83a7c9
Oleg Karnaukhov <allgenl@mail.ru>
2024-12-17 11:34:41 +0300

    Deploy gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/mindbox/',
   plugins: [react()],
-  test: {
-    globals: true, // Включаем глобальные функции, такие как describe, test, expect
-    environment: 'jsdom', // Устанавливаем среду для работы с DOM
-    setupFiles: './src/test/setup.ts', // Настройка перед тестами
-  },
 });
